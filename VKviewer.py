10ddece6433c713db7942b31e960071df5469941
Artyom <artyomrodimov@yandex.ru>
2018-05-31 18:55:33 +0300

    VKviewer.py fixed json problem

--- VKviewer.py
+++ VKviewer.py
@@ -15,45 +15,45 @@ def getdata():
 #Анализ полученных данных 
 def messages():
     m = getdata()
-    if len(m['updates']) > 1:
-        if len(m['updates'][1]) == 8:
-            r = m['updates'][1][2]
+    if len(m['updates']) >= 1:
+        if len(m['updates'][0]) == 8:
+            r = m['updates'][0][2]
             c = ''
             while r != 0:
                 c = str(r % 2) + c
-                r //= 2
+                r //= 2    
             if len(c) < 10:
                 c = ('0' * (10 - len(c))) + c # C это флаг сообщения в двоичном коде. Например: 1000000011 означает, что сообщение исходящее, не прочитанное, и сожержит медиаконтент см: https://vk.com/dev/using_longpoll_2
             if c[-2] == '0':
-                if m['updates'][1][3] < 2000000000: # 2000000000 + id это сообщение из беседы
-                    if m['updates'][1][3] > 0: # id Это сообщения из ЛС
-                        vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                        user = vk.users.get(user_id = m['updates'][1][3])
-                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + user[0]['first_name'] + ' ' + user[0]['last_name'] + '(ID - ' + str(m['updates'][1][3]) + '): ' + m['updates'][1][6]
-                    if m['updates'][1][3] < 0: # -id Это сообщения от сообществ
-                        vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                        group = vk.groups.getById(group_id = m['updates'][1][3] * (-1))
-                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + 'Сообщество ' + group[0]['name']  + '(ID - ' + str(m['updates'][1][3] * (-1)) + '): ' + m['updates'][1][6]
+                if m['updates'][0][3] < 2000000000: # 2000000000 + id это сообщение из беседы
+                    if m['updates'][0][3] > 0: # id Это сообщения из ЛС
+                        vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                        user = vk.users.get(user_id = m['updates'][0][3])
+                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + user[0]['first_name'] + ' ' + user[0]['last_name'] + '(ID - ' + str(m['updates'][0][3]) + '): ' + m['updates'][0][6]
+                    if m['updates'][0][3] < 0: # -id Это сообщения от сообществ
+                        vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                        group = vk.groups.getById(group_id = m['updates'][0][3] * (-1))
+                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + 'Сообщество ' + group[0]['name']  + '(ID - ' + str(m['updates'][0][3] * (-1)) + '): ' + m['updates'][0][6]
                 else: # Это беседа ВК
-                    vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                    userid = int(m['updates'][1][7]['from'])
+                    vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                    userid = int(m['updates'][0][7]['from'])
                     groupchat_user = vk.users.get(user_id = userid, name_case = 'ins')
                     groupchat_user = groupchat_user[0]['first_name'] + ' ' + groupchat_user[0]['last_name']
-                    message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + 'Отправлено из беседы ' + m['updates'][1][5] + '(ID: ' + str(m['updates'][1][3])+ ')' + ' ' + groupchat_user + ' (ID: '+ str(userid)+ ' )' + ': ' + m['updates'][1][6]
+                    message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + 'Отправлено из беседы ' + m['updates'][0][5] + '(ID: ' + str(m['updates'][0][3])+ ')' + ' ' + groupchat_user + ' (ID: '+ str(userid)+ ' )' + ': ' + m['updates'][0][6]
             else: # Это исходящее сообщение
-                if m['updates'][1][3] < 2000000000:
-                    if m['updates'][1][3] > 0: # Исходящее в ЛС
-                        vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                        user = vk.users.get(user_id = m['updates'][1][3], name_case='dat')
-                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + 'Вы отправили сообщение ' + user[0]['first_name'] + ' ' + user[0]['last_name'] + '(ID - ' + str(m['updates'][1][3]) + '): ' + m['updates'][1][6]
-                    if m['updates'][1][3] < 0: # Исходящее в Сообщество
-                        vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                        group = vk.groups.getById(group_id = m['updates'][1][3] * (-1))
-                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + 'Вы отправили сообщение сообществу ' + group[0]['name'] + '(ID - ' + str(m['updates'][1][3] * (-1)) + '): ' + m['updates'][1][6]
+                if m['updates'][0][3] < 2000000000:
+                    if m['updates'][0][3] > 0: # Исходящее в ЛС
+                        vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                        user = vk.users.get(user_id = m['updates'][0][3], name_case='dat')
+                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + 'Вы отправили сообщение ' + user[0]['first_name'] + ' ' + user[0]['last_name'] + '(ID - ' + str(m['updates'][0][3]) + '): ' + m['updates'][0][6]
+                    if m['updates'][0][3] < 0: # Исходящее в Сообщество
+                        vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                        group = vk.groups.getById(group_id = m['updates'][0][3] * (-1))
+                        message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + 'Вы отправили сообщение сообществу ' + group[0]['name'] + '(ID - ' + str(m['updates'][0][3] * (-1)) + '): ' + m['updates'][0][6]
                 else: # Исходящее в беседу
-                    vk.messages.markAsRead(message_ids = m['updates'][1][1])
-                    userid = int(m['updates'][1][7]['from'])
-                    message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][1][4])) + ' | ' + 'Сообщение отправлено вами в беседу ' + m['updates'][1][5] +' (ID: ' + str(m['updates'][1][3])+ ')' + ': ' + m['updates'][1][6]
+                    vk.messages.markAsRead(message_ids = m['updates'][0][1])
+                    userid = int(m['updates'][0][7]['from'])
+                    message = '| ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(m['updates'][0][4])) + ' | ' + 'Сообщение отправлено вами в беседу ' + m['updates'][0][5] +' (ID: ' + str(m['updates'][0][3])+ ')' + ': ' + m['updates'][0][6]
         else:
             message = 'Нет новых сообщений'
     else:
